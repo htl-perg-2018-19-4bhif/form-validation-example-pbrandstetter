aaefa402e5c398caa09702fbb9cbe2e6d757d27f
Philipp Brandstetter <philipp.brandstetter57@gmail.com>
2018-11-13 20:52:09 +0100

    Added validation to existing site

---
+++ app.ts
@@ -0,0 +1,61 @@
+window.onload = function () {
+    $('#emailMandatory').hide();
+    $('#otherMediaChannel').hide();
+    submitButton();
+
+    $('#firstName').on('input', function () {
+        if ($('#firstName').val()) {
+            $('#firstNameMandatory').hide();
+        } else {
+            $('#firstNameMandatory').show();
+        }
+        submitButton();
+    });
+
+    $('#lastName').on('input', function () {
+        if ($('#lastName').val()) {
+            $('#lastNameMandatory').hide();
+        } else {
+            $('#lastNameMandatory').show();
+        }
+        submitButton();
+    });
+
+    $('#email').on('input', function () {
+        if ($('#email').val()) {
+            $('#emailMandatory').hide();
+        } else if (!$('#email').val() && $('#newsletter').is(':checked')) {
+            $('#emailMandatory').show();
+        }
+        submitButton();
+    });
+
+    $('#newsletter').click(function () {
+        if ($('#newsletter').is(':checked') && $('#email').val()) {
+            $('#emailMandatory').hide();
+        } else if ($('#newsletter').is(':checked') && !$('#email').val()) {
+            $('#emailMandatory').show();
+        } else {
+            $('#emailMandatory').hide();
+        }
+        submitButton();
+    });
+
+    $('#mediaChannelSelect').click(function () {
+        if ($('#mediaChannelSelect').find(":selected").text() === "Other") {
+            $('#otherMediaChannel').show();
+        } else {
+            $('#otherMediaChannel').hide();
+        }
+        submitButton();
+    });
+
+    function submitButton() {
+        if (($('#firstName').val() && $('#lastName').val() && !$('#newsletter').is(':checked') && !$('#email').val()) ||
+            ($('#firstName').val() && $('#lastName').val() && $('#newsletter').is(':checked') && $('#email').val())) {
+            $(":submit").prop("disabled", false);
+        } else {
+            $(":submit").prop("disabled", true);
+        }
+    }
+}
